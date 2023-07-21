40c8f1911b7f6e3c9fa060f2812dbfb9bf885996
Ekaterina Markeeva <lesekaterina25@gmail.com>
2023-07-21 14:54:41 +0400

    homework poll повышенный уровень

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,5 +1,6 @@
 const pollTitle = document.getElementById('poll__title');
 const pollAnswers = document.getElementById('poll__answers');
+const card = document.querySelector('.card');
 const xhr = new XMLHttpRequest();
 
 xhr.addEventListener('readystatechange', () => {
@@ -15,7 +16,27 @@ xhr.addEventListener('readystatechange', () => {
 
             btnsAnswer[btnsAnswer.length - 1].addEventListener('click', () => {
                 alert('Спасибо, ваш голос засчитан!');
+                pollAnswers.remove();
 
+                const xhrPost = new XMLHttpRequest();
+                xhrPost.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
+                xhrPost.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
+                xhrPost.send(`vote=${xhrResponse.id}&answer=${btnsAnswer.length - 1}`);
+                xhrPost.addEventListener('readystatechange', () => {
+                    if (xhrPost.readyState === xhr.DONE) {
+                        const xhrPostResponse = JSON.parse(xhrPost.response);
+                        console.log(xhrPostResponse.stat);
+                        
+                        for (let elem of xhrPostResponse.stat) {
+                            card.insertAdjacentHTML('beforeend', '<div class="stat"><div class="stat__answer"></div><div class="stat__votes"></div></div>');
+                            const answers = Array.from(document.querySelectorAll('.stat__answer'));
+                            const votes = Array.from(document.querySelectorAll('.stat__votes'));
+
+                            answers[answers.length - 1].textContent = elem.answer + ':';
+                            votes[votes.length - 1].textContent = elem.votes + '%';
+                        }
+                    }
+                });
             });
         }
     }
